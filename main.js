0730bfd8a95602a942ac28ca2cfc1368a06300a3
Essmea <maiaruiz@live.se>
2025-03-18 22:02:48 +0100

    if och for

--- main.js
+++ main.js
@@ -4,7 +4,19 @@ const app = Vue.createApp ({
             product: 'Socks',
             description: 'This is our product description',
             image: './assets/images/socks_green.jpg',
-            url:'https://github.com/Essmea/Intro-to-Vue-3'
+            url:'https://github.com/Essmea/Intro-to-Vue-3',
+            inventory: 11,
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20%% polyester'],
+            variants: [
+                {id: 2234, color:'Green'},
+                {id: 2235, color:'Blue'}
+            ],
+            sizes: [
+                {id:1, size:"S"},
+                {id:2, size:"M"},
+                {id:3, size:"L"}
+            ]
         }
     }
 })

0117b01cd09cb8877b2854a9bb69824645286546
Essmea <maiaruiz@live.se>
2025-03-18 21:38:12 +0100

    second commit

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp ({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            description: 'This is our product description',
+            image: './assets/images/socks_green.jpg',
+            url:'https://github.com/Essmea/Intro-to-Vue-3'
         }
     }
 })

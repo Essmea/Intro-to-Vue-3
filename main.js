69cc017ee42be4cba8202e194145ea0dcc989605
Essmea <maiaruiz@live.se>
2025-03-18 23:18:43 +0100

    commit

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp ({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
             description: 'This is our product description',
             image: './assets/images/socks_green.jpg',
@@ -9,14 +10,28 @@ const app = Vue.createApp ({
             onSale: true,
             details: ['50% cotton', '30% wool', '20%% polyester'],
             variants: [
-                {id: 2234, color:'Green'},
-                {id: 2235, color:'Blue'}
+                {id: 2234, color:'green', image: './assets/images/socks_green.jpg'},
+                {id: 2235, color:'blue', image: './assets/images/socks_blue.jpg'}
             ],
             sizes: [
                 {id:1, size:"S"},
                 {id:2, size:"M"},
                 {id:3, size:"L"}
-            ]
+            ],
+            inStock:false
+        }
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1
+        },
+        removeFromCart(){
+            if (this.cart > 0){ 
+                this.cart -= 1
+            }
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 })
